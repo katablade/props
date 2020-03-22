c6889d610bbc98a52e226f70b672676366afdd40
katablade <57857686+katablade@users.noreply.github.com>
2020-03-22 11:25:41 +0100

    Update Title.js

--- src/Title.js
+++ src/Title.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 function Title (props){
+    let small = (props.small)? "18px": "35px";
     return(
-    <h1>{props.children}</h1>
+    <h1 style={{fontSize: small}}>{props.children}</h1>
     );
 }
 export default Title;
